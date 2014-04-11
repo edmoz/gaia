8fb577c96cccf14ecdceaa58746b674cfbb6b6e3
rpappalax <rpappalax@gmail.com>
2014-04-10 22:47:22 -0700

    adding testcase for test-fxa-client

---
+++ test_apps/test-fxa-client/test/marionette/fxa_user_new_test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var FxA = require('./lib/fxa');
+var assert = require('assert');
+
+marionette('Launch Test FxA Client', function() {
+  var app;
+  var selectors;
+  var client = marionette.client();
+
+  setup(function() {
+    app = new FxA(client);
+    selectors = FxA.Selectors;
+    app.launch();
+    //app.selectFxaTest();
+    app.selectFxaClientTest();
+
+  });  // end: setup
+
+  test('should enter (valid) credentials - for new user', function () {
+
+    assert.ok(app.typeEmail(FxA.EMAIL) !== -1);
+    assert.ok(app.onClick(selectors.moduleNext) !== -1);
+    assert.ok(app.typePassword(FxA.PASSWORD) !== -1);
+    assert.ok(app.onClick(selectors.moduleNext) !== -1);
+    assert.ok(app.onClick(selectors.errorOK) !== -1);
+
+    //app.dumpPageSource();
+  });
+
+});  // end: marionette
+
+
+
